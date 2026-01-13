686bd4fc244ce385899583967af3dbc37ae60623
Jack Lovett <jack.l.lovett@gmail.com>
2026-01-13 07:43:07 +0000

    Convert forgot password page to an AuthPage

--- app/routes/$auth.forgot-password.tsx
+++ app/routes/$auth.forgot-password.tsx
@@ -3,10 +3,9 @@ import {
   data,
   Link,
   MetaFunction,
-  useNavigation,
 } from 'react-router'
 
-import { FormComponent, FormInput, Page } from '~/components'
+import { AuthPage, FormComponent, FormInput } from '~/components'
 import { post } from '~/services/networkService'
 import { useFormValidation, useSubmitStatus } from '~/hooks'
 import { validateEmail } from '~/hooks/useFormValidation'
@@ -66,9 +65,6 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 }
 
 const ForgotPassword = () => {
-  const { state } = useNavigation()
-  const loading = state === 'loading'
-
   const submitStatus = useSubmitStatus()
 
   const { formState, errors, isFormValid, handleChange } = useFormValidation({
@@ -79,46 +75,39 @@ const ForgotPassword = () => {
   })
 
   return (
-    <Page>
-      <div className="center column mt">
-        <img
-          src="/images/png/logo-no-text.png"
-          width="160"
-          alt="Surf spots logo"
-        />
-        <div className="auth-title">
-          <h1>Forgot Password?</h1>
-        </div>
-        <div className="page-content center-text">
-          <p>
-            Don't worry, enter your email address to receive reset instructions.
-          </p>
-          <FormComponent
-            isDisabled={!isFormValid}
-            submitLabel="Send Reset Email"
-            submitStatus={submitStatus}
-          >
-            <FormInput
-              field={{
-                label: 'Email',
-                name: 'email',
-                type: 'email',
-                validationRules: { required: true },
-              }}
-              value={formState.email}
-              onChange={(e) => handleChange('email', e.target.value)}
-              errorMessage={errors.email || ''}
-              showLabel={!!formState.email}
-            />
-          </FormComponent>
-          <div className="mt center-horizontal font-small">
-            <Link to="/auth" prefetch="intent">
-              Back to login
-            </Link>
-          </div>
+    <AuthPage>
+      <div className="auth-title">
+        <h1>Forgot Password?</h1>
+      </div>
+      <div className="page-content mt">
+        <p className="center-text">
+          Don't worry, enter your email address to receive reset instructions.
+        </p>
+        <FormComponent
+          isDisabled={!isFormValid}
+          submitLabel="Send Reset Email"
+          submitStatus={submitStatus}
+        >
+          <FormInput
+            field={{
+              label: 'Email',
+              name: 'email',
+              type: 'email',
+              validationRules: { required: true },
+            }}
+            value={formState.email}
+            onChange={(e) => handleChange('email', e.target.value)}
+            errorMessage={errors.email || ''}
+            showLabel={!!formState.email}
+          />
+        </FormComponent>
+        <div className="mt center-horizontal font-small">
+          <Link to="/auth" prefetch="intent">
+            Back to login
+          </Link>
         </div>
       </div>
-    </Page>
+    </AuthPage>
   )
 }
 
