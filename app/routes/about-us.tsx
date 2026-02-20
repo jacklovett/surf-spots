31b5877e243347b6559098d4cb404703632e1325
Jack Lovett <jack.l.lovett@gmail.com>
2026-02-20 09:54:55 +0000

    Remove unnecessary class names from headings

--- app/routes/about-us.tsx
+++ app/routes/about-us.tsx
@@ -4,12 +4,12 @@ export default function AboutUs() {
   return (
     <Page showHeader>
       <div className="info-page-content mv">
-        <h1 className="mb">About Surf Spots</h1>
+        <h1>About Surf Spots</h1>
 
         <div className="column gap">
           <section>
-            <h2 className="mb">The Story</h2>
-            <p className="mb">
+            <h2>The Story</h2>
+            <p>
               Surf Spots was created by a passionate surfer who wanted a way to
               keep track of the spots he has exlored around the world. Where I
               have surfed, which countries and continents etc. I thought maybe
@@ -20,8 +20,8 @@ export default function AboutUs() {
           </section>
 
           <section>
-            <h2 className="mb">Support the Project</h2>
-            <p className="mb">
+            <h2>Support the Project</h2>
+            <p>
               If you find Surf Spots helpful, consider supporting the project!
               Your support helps keep the platform running and allows me to add
               more features and improvements.
@@ -32,7 +32,7 @@ export default function AboutUs() {
           </section>
 
           <section>
-            <h2 className="mb">Affiliate Links</h2>
+            <h2>Affiliate Links</h2>
             <p>
               Some links to surf gear and related products on this site are
               affiliate links. This means if you make a purchase through these
@@ -42,7 +42,7 @@ export default function AboutUs() {
           </section>
 
           <section>
-            <h2 className="mb">Direct Advertisement</h2>
+            <h2>Direct Advertisement</h2>
             <p>
               If you are a surf or ocean related business that would like to
               advertise on the platform then reach out at:{' '}
